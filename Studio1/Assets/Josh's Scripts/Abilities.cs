99fda96cdcc0aeef773a0eb62203a6f140ac953c
Meraki-Destric <67037310+Meraki-Destric@users.noreply.github.com>
2020-09-30 11:37:52 +1000

    Basic Player Controller
    Create the framework for the controller

---
+++ Studio1/Assets/Josh's Scripts/Abilities.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abilities : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int abiCoolDown1 = 3;
+        int abiCoolDown2 = 5;
+    }
+
+    void Ability1()
+    {
+
+    }
+
+    void Ability2()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+    }
+}
--- Studio1/Assets/Josh's Scripts/PlayerKeyController.cs
+++ Studio1/Assets/Josh's Scripts/PlayerKeyController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerKeyController : MonoBehaviour
 {
+    public Abilities abilities;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
+
     }
 }
