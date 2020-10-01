30592dbce2ddb1f3fbc669716cfd120dcc054457
Meraki-Destric <67037310+Meraki-Destric@users.noreply.github.com>
2020-10-01 20:39:40 +1000

    Karma Compatibility Added + 10 other ability slots + Camera Follow

--- Studio1/Assets/Josh's Scripts/Abilities.cs
+++ Studio1/Assets/Josh's Scripts/Abilities.cs
@@ -11,6 +11,16 @@ public class Abilities : MonoBehaviour
     //CoolDown Length Variables
     public int abiCoolDown1 = 3;
     public int abiCoolDown2 = 5;
+    public int abiCoolDown3 = 3;
+    public int abiCoolDown4 = 5;
+    public int abiCoolDown5 = 3;
+    public int abiCoolDown6 = 5;
+    public int abiCoolDown7 = 3;
+    public int abiCoolDown8 = 5;
+    public int abiCoolDown9 = 3;
+    public int abiCoolDown10 = 5;
+    public int abiCoolDown11 = 3;
+    public int abiCoolDown12 = 5;
 
     //Ability 1 Variables
     float jumpForce = 1000f;
@@ -33,5 +43,55 @@ public class Abilities : MonoBehaviour
     public void Ability2()
     {
         Debug.Log("Ability 2 Used");
+    }
+
+    public void Ability3()
+    {
+        Debug.Log("Ability 3 Used");
+    }
+
+    public void Ability4()
+    {
+        Debug.Log("Ability 4 Used");
+    }
+
+    public void Ability5()
+    {
+        Debug.Log("Ability 5 Used");
+    }
+
+    public void Ability6()
+    {
+        Debug.Log("Ability 6 Used");
+    }
+
+    public void Ability7()
+    {
+        Debug.Log("Ability 7 Used");
+    }
+
+    public void Ability8()
+    {
+        Debug.Log("Ability 8 Used");
+    }
+
+    public void Ability9()
+    {
+        Debug.Log("Ability 9 Used");
+    }
+
+    public void Ability10()
+    {
+        Debug.Log("Ability 10 Used");
+    }
+
+    public void Ability11()
+    {
+        Debug.Log("Ability 11 Used");
+    }
+
+    public void Ability12()
+    {
+        Debug.Log("Ability 12 Used");
     }
 }
