069d5ca5a70e50eafeb424e6ece3cebd6dd1d92f
Meraki-Destric <67037310+Meraki-Destric@users.noreply.github.com>
2020-10-01 12:28:53 +1000

    Basic Box Controller and Abilities
    Because the way the player controller works will probably need a big overhaul once we have our model. For now, this allows you to move a basic cube about and also 1 ability that launches you up in the air

--- Studio1/Assets/Josh's Scripts/Abilities.cs
+++ Studio1/Assets/Josh's Scripts/Abilities.cs
@@ -5,29 +5,33 @@ using UnityEngine;
 
 public class Abilities : MonoBehaviour
 {
+    //References
+    public Rigidbody rb;
+
+    //CoolDown Length Variables
+    public int abiCoolDown1 = 3;
+    public int abiCoolDown2 = 5;
+
+    //Ability 1 Variables
+    float jumpForce = 1000f;
+
     // Start is called before the first frame update
     void Start()
     {
-        int abiCoolDown1 = 3;
-        int abiCoolDown2 = 5;
-
-    }
-
-    void Ability1()
-    {
-
-    }
-
-    void Ability2()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
 
+    }
 
+    //Test Jump Ability
+    public void Ability1()
+    {
+        Debug.Log("Ability 1 used");
+
+        rb.AddForce(0, jumpForce, 0);
+    }
+
+    public void Ability2()
+    {
+        Debug.Log("Ability 2 Used");
     }
 }
--- Studio1/Assets/Josh's Scripts/PlayerKeyController.cs
+++ Studio1/Assets/Josh's Scripts/PlayerKeyController.cs
@@ -1,50 +1,117 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerKeyController : MonoBehaviour
 {
+    //References
     public Abilities abilities;
+    public Rigidbody rb;
+
+    //Timers
+    float timer1 = 0;
+    float timer2 = 0;
+
+
+    //Movement Speeds
+    public float forwardForce = 2000f;
+    public float backForce = -2000f;
+    public float leftForce = -2000f;
+    public float rightForce = 2000f;
+
+    public float airMovementSpeed = 200f;
+
+    //Checkers
+    public bool grounded = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        float timer1 = 0;
-        float timer2 = 0;
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Checks if the gameObject is touching the floor
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+
+        else
+        {
+            grounded = false;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W))
+        //Movement Commands
+        //Checks in the player is grounded before allowing them to move
+        if (grounded == true)
+        { 
+            if (Input.GetKey(KeyCode.W))
+            {
+                //The brackets contain x, y, z
+                //X refers to left and right (generally) and z, front and back
+                //Y refers to height e.g Jumping
+                rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.AddForce(leftForce * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.AddForce(0, 0, backForce * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.AddForce(rightForce * Time.deltaTime, 0, 0);
+            }
+        }
+
+        if (grounded == false)
         {
 
         }
+        
+        //Ability Activations
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.Q) && timer1 < 0)
         {
+            //Causes Ability
+            abilities.Ability1();
 
+            //Abilty CoolDown
+            timer1 = abilities.abiCoolDown1;          
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (timer1 > -1)
         {
-
+            timer1 -= 1 * Time.deltaTime;
+            Debug.Log(timer1);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.E) && timer2 < 0)
         {
+            //Causes Ability
+            abilities.Ability2();
 
+
+            //Abilty CoolDown
+            timer2 = abilities.abiCoolDown2;
         }
 
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (timer2 >= 0)
         {
-
+            timer2 -= 1 * Time.deltaTime;
+            Debug.Log(timer2);
         }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-
-        }
-
     }
 }
