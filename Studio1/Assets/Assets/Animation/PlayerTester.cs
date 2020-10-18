789c627722e4f790cd40f6753eafa525636a4eb2
Sophia-McNee <Flowerkirby@gmail.com.au>
2020-10-18 15:04:15 +1100

    SophiAni

---
+++ Studio1/Assets/Assets/Animation/PlayerTester.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTester : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+    }
+}
