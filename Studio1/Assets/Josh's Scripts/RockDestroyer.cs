abc4b58c0a7083a0cf0e955e3bf651dcf52028cb
Meraki-Destric <67037310+Meraki-Destric@users.noreply.github.com>
2020-10-21 22:29:50 +1100

    Save on Progress

--- Studio1/Assets/Josh's Scripts/RockDestroyer.cs
+++ Studio1/Assets/Josh's Scripts/RockDestroyer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Policy;
 using UnityEngine;
 
 public class RockDestroyer : MonoBehaviour
@@ -7,22 +8,46 @@ public class RockDestroyer : MonoBehaviour
     public GuardianController guardian;
     public Rigidbody rb;
 
+    public float timer = 5;
+    public bool collisionReached = false;
+    public bool changedOnce = false;
+
     public void Update()
     {
         if (gameObject.name == "Flying Rock")
         {
+
             GameObject guardianController = GameObject.Find("Guardian Controller");
             guardian = guardianController.GetComponent<GuardianController>();
             Quaternion rotation = guardian.playerRotation;
             Vector3 direction = rotation * Vector3.forward;
+
             transform.position += direction * Time.deltaTime * 100;
+
+            if (changedOnce == false)
+            {
+                transform.rotation = rotation;
+                changedOnce = true;
+            }
         }
+
+        if (collisionReached == false)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
     }
 
     public void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.layer == 0 && col.gameObject.name != "Flying Rock")
         {
+            collisionReached = true;
             GameObject guardianController = GameObject.Find("Guardian Controller");
             guardian = guardianController.GetComponent<GuardianController>();
 
