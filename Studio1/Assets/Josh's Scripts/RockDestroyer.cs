e709d3f42837f85e2096f3699245888ec1711c13
Meraki-Destric <67037310+Meraki-Destric@users.noreply.github.com>
2020-10-21 11:13:51 +1100

    Rocks fly in direction player is facing

--- Studio1/Assets/Josh's Scripts/RockDestroyer.cs
+++ Studio1/Assets/Josh's Scripts/RockDestroyer.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class RockDestroyer : MonoBehaviour
 {
     public GuardianController guardian;
+    public Rigidbody rb;
+
+    public void Update()
+    {
+        if (gameObject.name == "Flying Rock")
+        {
+            GameObject guardianController = GameObject.Find("Guardian Controller");
+            guardian = guardianController.GetComponent<GuardianController>();
+            Quaternion rotation = guardian.playerRotation;
+            Vector3 direction = rotation * Vector3.forward;
+            transform.position += direction * Time.deltaTime * 100;
+        }
+    }
 
     public void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.layer == 0)
+        if (col.gameObject.layer == 0 && col.gameObject.name != "Flying Rock")
         {
+            GameObject guardianController = GameObject.Find("Guardian Controller");
+            guardian = guardianController.GetComponent<GuardianController>();
+
             guardian.flyingRock = false;
             Destroy(gameObject);
         }
