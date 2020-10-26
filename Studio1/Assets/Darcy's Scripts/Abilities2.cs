f3236057249177587b391142db3a9afa75ab1f60
darcyglover <darcyglover@users.noreply.github.com>
2020-10-27 09:20:22 +1100

    animosity

---
+++ Studio1/Assets/Darcy's Scripts/Abilities2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ParticleSystemJobs;
+
+public class Abilities2 : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private GameObject strike, earthen, qAbilityActive, eAbilityActive;
+
+    public void StrikeCast()
+    {
+        Destroy(qAbilityActive); //destroying the previous instantiated objects, if any
+        qAbilityActive = Instantiate(strike, player.position, player.rotation); //creating the ability
+    }
+
+    public void EarthenCast()
+    {
+        Destroy(eAbilityActive);
+        eAbilityActive = Instantiate(earthen, player.position, Quaternion.identity);
+    }
+
+    void OnParticleCollision(GameObject other) //if the ability hits something with the enemy layer it destroys it
+    {
+        Destroy(other);
+    }
+}
