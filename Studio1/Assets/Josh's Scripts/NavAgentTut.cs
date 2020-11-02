2e45565c61e82e305b3327fd05278f3a16647fee
darcyglover <darcyglover@users.noreply.github.com>
2020-11-02 16:53:16 +1100

    revert pushformitch

---
+++ Studio1/Assets/Josh's Scripts/NavAgentTut.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//Means this script won't run without a playermotor script
+[RequireComponent(typeof(PlayerMotor))]
+public class NavAgentTut : MonoBehaviour
+{
+    public PlayerMotor motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+        }
+    }
+
+}
---
+++ Studio1/Assets/Josh's Scripts/Redundant/RockDestroyer.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+
+public class RockDestroyer : MonoBehaviour
+{
+    public GuardianController guardian;
+    public Rigidbody rb;
+    public ConvictionCalculator conviction;
+
+    private GameObject currentEnemy; 
+
+    public float timer = 3;
+    public bool collisionReached = false;
+    public bool hitEnemy = false;
+
+
+    public void Awake()
+    {
+        collisionReached = false;
+
+        //If there are errors, ensure that the gameObject holding the guardian controller is named like this
+        GameObject guardianController = GameObject.Find("Guardian Controller");
+        guardian = guardianController.GetComponent<GuardianController>();
+    }
+
+    public void Update()
+    {
+        if (gameObject.name == "Flying Rock")
+        {
+            
+
+            Quaternion rotation = guardian.playerRotation;
+            Vector3 direction = rotation * Vector3.forward;
+
+            transform.position += direction * Time.deltaTime * 100;
+        }
+
+        if (collisionReached == false)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                guardian.flyingRock = false;
+                Destroy(gameObject);
+            }
+        }
+
+        if (currentEnemy != null)
+        {
+            if (hitEnemy)
+            {
+
+                hitEnemy = false;
+            }
+        }
+
+    }
+
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.layer == 0 && col.gameObject.name != "Flying Rock")
+        {
+            guardian.flyingRock = false;
+            Destroy(gameObject);
+        }
+
+        if (col.gameObject.CompareTag("Blade"))
+        {
+            currentEnemy = col.gameObject;
+            currentEnemy.GetComponent<BladeAI>().bladeHealth -= 1;
+            hitEnemy = true;
+            guardian.flyingRock = false;
+            Destroy(gameObject);
+        }
+
+        if (col.gameObject.CompareTag("Player"))
+        {
+            guardian.antiMouseLock = true;
+            conviction.convictionCount -= 10;
+            guardian.flyingRock = false;
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            guardian.flyingRock = false;
+            Destroy(gameObject);
+        }
+    }
+}
