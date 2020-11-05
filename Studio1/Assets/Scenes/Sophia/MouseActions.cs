a530a8c005487e5da9786f3306d35f5685720a20
Sophia-McNee <Flowerkirby@gmail.com.au>
2020-11-05 21:47:53 +1100

    NPC added, Cloak added, My own Scenes moved to another folder, more scenes added for testing models

---
+++ Studio1/Assets/Scenes/Sophia/MouseActions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseActions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Color defaultColour;
+    public Color hoverColour;
+    public Color ClickColour;
+    private Renderer renderer;
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        renderer.material.color = defaultColour;
+    }
+
+    private void OnMouseEnter()
+    {
+        renderer.material.color = hoverColour;
+    }
+
+    private void OnMouseExit()
+    {
+        renderer.material.color = defaultColour;
+    }
+
+    private void OnMouseDown()
+    {
+        renderer.material.color = ClickColour;
+    }
+
+    private void OnMouseUp()
+    {
+        renderer.material.color = hoverColour;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
