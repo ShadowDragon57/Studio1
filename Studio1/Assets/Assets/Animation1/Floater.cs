aba06c36b2aeef2636c5e16d916c3308beb3bf31
SophiaM <Flowerkirby@gmail.com>
2020-11-27 20:03:52 +1100

    NPC

---
+++ Studio1/Assets/Assets/Animation1/Floater.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Make the object float up and down while spinning gently
+public class Floater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // User Input
+    public float degreesPerSecond = 15.0f; //Spinning speed
+    public float amplitude = 0.5f; //Height
+    public float frequency = 1f; //Movement speed
+
+    //Position Storage Variables
+    Vector3 posOffset = new Vector3 ();
+    Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        //Store the starting position and rotation of the object
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spin Object on y-Axis
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+
+        //Float up/down with Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
