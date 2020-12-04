23cde6da9d943714d97cd7583125834128f5ec6b
Darcy Glover <darcy.glover414@gmail.com>
2020-12-04 22:59:21 +1100

    sound manager testing

---
+++ Studio1/Assets/Darcy's Scripts/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip test;
+    public void PlaySound()
+    {
+        GameObject soundGameObject = new GameObject("Sound");
+        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+        audioSource.PlayOneShot(test);
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Return))
+        {
+            PlaySound();
+        }
+    }
+}
